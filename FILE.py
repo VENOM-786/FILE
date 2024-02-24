944f91f18a2c69dd3a2b7679bafb4b8d1ae84da5
Usman Rajpoot <113978329+VENOM-786@users.noreply.github.com>
2024-02-24 05:36:37 +0500

    Create FILE.py

---
+++ FILE.py
@@ -0,0 +1,7 @@
+import os,platform
+os.system('git pull -q')
+#exit('\n Update Soon...!')
+VENOM=platform.architecture()[0]
+if VENOM=="32bit":__import__("FELY32")
+elif VENOM=="64bit":__import__("FELY")
+ 
